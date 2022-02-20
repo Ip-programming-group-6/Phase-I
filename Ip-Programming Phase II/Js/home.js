86874f1c2aa6954dffe1bf7743130149e6f30aa0
Jima Dube <73731892+Jima-J9M1@users.noreply.github.com>
2022-02-20 10:06:18 +0300

    Add files via upload

--- Ip-Programming Phase II/Js/home.js
+++ Ip-Programming Phase II/Js/home.js
@@ -1,5 +1,4 @@
-// Toggle the link bar for responsivness 
-// To Do that active the link-bar 
+
 const btn=document.getElementById("toggleButton");
 const link=document.getElementsByClassName("navbar")[0];
 
@@ -11,20 +10,4 @@ btn.addEventListener('click',()=>{
 
 
 
-// <div class="gallery-item">
-// <div class="image">
-//     <img src="http://source.unsplash.com//600x400/?orphanage" alt="food">
-// </div>
-// <div class="item">
-//     <div class="text">
-//         <h2>Title</h2>
-//         <div class="paragraph">Description goes here </div>
-//     </div>
-//     <progress class="progress" max="140000" value="70000"></progress>
-//     <div class="button">
-//         <button class="D-S donate">Donate</button>
-//         <button class="D-S share">Share</button>
-//     </div>
-// </div>
-// </div>
 
